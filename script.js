a84c4fa20249f6b0bfd2869a0352cd50c0e0dafa
kataleya10 <136022898+kataleya10@users.noreply.github.com>
2023-09-17 18:27:37 +0200

    added js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hola")
+var dice = {
+    sides: 6
+    roll: function () {
+        var randomNumber = math.floor(math.random() * this.sides) + 1;
+        return randomNumber;
+    }
+}
+function printNumber(number) {
+    var placeholder = document.getElementById("placeholder");
+    placeholder.innerHTML = number;
+}
+var button = document.getElementById(button);
+button.onclick = function () {
+    var result = dice.roll();
+    printNumber(result);
+
+}
